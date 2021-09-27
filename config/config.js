135384cde6429113cb14f90fc493f3487aec5b29
MinBeomGi <alsqjarlwkd@naver.com>
2021-09-27 16:07:47 +0900

    port change

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 export const backUrl =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:8081" //배포 백엔드 주소
+    ? "http://bilabcsapi.lunalabs.net" //배포 백엔드 주소
     : "http://localhost:8081"; //개발 백엔드 주소
