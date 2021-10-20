c73dd2a77d5784afb32395c482c2f5cb95246114
강민규 <alsrb9434@gmail.com>
2021-10-20 22:30:36 +0900

    test4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   apps: [
     {
+      name: "bilab",
       script: "node_modules/next/dist/bin/next",
-      args: "start",
+      args: "start -p 3300",
       exec_mode: "cluster",
+      instances: 0,
       watch: true,
       env_production: {
         PORT: 3300,
