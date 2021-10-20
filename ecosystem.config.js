a7bab7d33a3268c4153d9680d79b3f9a666d4084
강민규 <alsrb9434@gmail.com>
2021-10-20 22:11:55 +0900

    test..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,11 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start",
       exec_mode: "cluster",
+      watch: true,
+      env_production: {
+        PORT: 3300,
+        NODE_ENV: "production",
+      },
     },
   ],
 };
